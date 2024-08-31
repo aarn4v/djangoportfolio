5577bc48f2836f607a60bfd8a0a5f6d6ac8747b9
Aarnav Gujjari <aarnavg@Aarnavs-MacBook-Air.local>
2024-08-30 19:19:24 -0700

    Fixed urls and html format

--- blog/models.py
+++ blog/models.py
@@ -9,3 +9,14 @@ class Blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
+# Blog post in admin is the title
+    def __str__(self):
+        return self.title
+
+# Caps summary to certain numbers of chars
+    def summary(self):
+        return self.body[:100]
+
+# Strftime to display date however
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
